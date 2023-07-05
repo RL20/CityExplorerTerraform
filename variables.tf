abbcc1d3cbcebd777165a91dc22c2091b51376ed
RL20 <harel_shalom@yahoo.com>
2023-07-05 22:52:37 +0300

    Add variables.tf file with AWS and RDS configurations

---
+++ variables.tf
@@ -0,0 +1,35 @@
+# Defines what variables you are using
+
+variable "region" {
+  description = "AWS Deployment region"
+  default = "us-west-2"
+}
+
+variable "keypair_name" {
+  description = "SSH Key"
+  sensitive = true
+}
+
+variable "api_key" {
+  description = "API Key for the weather api"
+  sensitive = true
+}
+# variable "iam" {
+#   description = "iam user"
+#   sensitive = true
+# }
+
+variable "snapshot_identifier" {
+  description = "RDS snapshot"
+  sensitive = true
+}
+
+variable "rds_username" {
+  description = "RDS username"
+  sensitive = true
+}
+
+variable "rds_password" {
+  description = "RDS password"
+  sensitive = true
+}
