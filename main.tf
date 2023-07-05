86969ba835d35ffc88bcc526dbf7ac266430adbe
RL20 <harel_shalom@yahoo.com>
2023-07-06 00:56:58 +0300

    Add Auto Scaling Policies

--- main.tf
+++ main.tf
@@ -108,3 +108,51 @@ resource "aws_db_instance" "city-explorer_db" {
     "name" = "city-explorer-db"
   }
 } 
+
+resource "aws_autoscaling_policy" "cityexplorer_policy_up" {
+  name                   = "cityexplorer_policy_up"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.cityexplorer_asg.name
+}
+
+resource "aws_cloudwatch_metric_alarm" "cityexplorer_cpu_alarm_up" {
+  alarm_name          = "cityexplorer_cpu_alarm_up"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "70"
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.cityexplorer_asg.name
+  }
+  alarm_description = "This metric monitor EC2 instance CPU utilization"
+  alarm_actions     = ["${aws_autoscaling_policy.cityexplorer_policy_up.arn}"]
+}
+
+resource "aws_autoscaling_policy" "cityexplorer_policy_down" {
+  name                   = "cityexplorer_policy_down"
+  scaling_adjustment     = -1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.cityexplorer_asg.name
+}
+
+resource "aws_cloudwatch_metric_alarm" "cityexplorer_cpu_alarm_down" {
+  alarm_name          = "cityexplorer_cpu_alarm_down"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "30"
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.cityexplorer_asg.name
+  }
+  alarm_description = "This metric monitor EC2 instance CPU utilization"
+  alarm_actions     = ["${aws_autoscaling_policy.cityexplorer_policy_down.arn}"]
+}
